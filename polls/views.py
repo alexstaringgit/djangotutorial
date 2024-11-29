825d2cfb8b840c9809b76e6967bc727b2c4239a9
alexstaringgit <alexstaring425@gmail.com>
2024-11-29 05:26:27 +0200

    complete part 3

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,4 @@
 from django.http import HttpResponse
-from django.http import Http404
-from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
 from .models import Question
